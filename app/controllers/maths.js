a4e108ef002188e1c2f819168057a8c74fdc11dd
Nicolas Conan <nicolas.conan.88@gmail.com>
2025-07-11 01:25:50 +0200

    dice and dice20

--- app/controllers/maths.js
+++ app/controllers/maths.js
@@ -1,8 +1,18 @@
 const mathsController = {
     randomBoolean(req, res) {
-        const v = Boolean(Math.floor(Math.random() * 2));
-        res.json({ prompt: "Random boolean", value: v });
+        const value = Boolean(Math.floor(Math.random() * 2));
+        res.json({ prompt: "Random boolean", value });
     },
+
+    randomDice(req, res) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        res.json({ prompt: "Random boolean", value });
+    },
+
+    randomDice20(req, res) {
+        const value = Math.floor(Math.random() * 20) + 1;
+        res.json({ prompt: "Random boolean", value });
+    }
 }
 
 export default mathsController;
--- app/router.js
+++ app/router.js
@@ -5,6 +5,9 @@ import cardController from "./controllers/card.js";
 const router = new Router();
 
 router.get('/randomBoolean', mathsController.randomBoolean);
+router.get('/randomDice', mathsController.randomDice);
+router.get('/randomDice20', mathsController.randomDice20)
+
 router.get('/randomCard', cardController.randomCard);
 router.get('/allCards', cardController.allCards);
 
